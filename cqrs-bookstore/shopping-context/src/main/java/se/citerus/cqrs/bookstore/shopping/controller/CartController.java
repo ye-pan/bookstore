d306ce9914d4e07b8e2be668953e4470fb026e5c
yepan <132842520@qq.com>
2019-05-25 23:30:55 +0800

    代码重构，引入CartService

--- cqrs-bookstore/shopping-context/src/main/java/se/citerus/cqrs/bookstore/shopping/controller/CartController.java
+++ cqrs-bookstore/shopping-context/src/main/java/se/citerus/cqrs/bookstore/shopping/controller/CartController.java
@@ -13,18 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import se.citerus.cqrs.bookstore.TransportObject;
 import se.citerus.cqrs.bookstore.api.ApiMessageDto;
 import se.citerus.cqrs.bookstore.shopping.api.AddItemRequest;
 import se.citerus.cqrs.bookstore.shopping.api.CartDto;
 import se.citerus.cqrs.bookstore.shopping.api.CartDtoFactory;
 import se.citerus.cqrs.bookstore.shopping.api.CreateCartRequest;
-import se.citerus.cqrs.bookstore.shopping.client.productcatalog.ProductCatalogClient;
-import se.citerus.cqrs.bookstore.shopping.client.productcatalog.ProductDto;
 import se.citerus.cqrs.bookstore.shopping.domain.Cart;
 import se.citerus.cqrs.bookstore.shopping.domain.CartRepository;
-import se.citerus.cqrs.bookstore.shopping.domain.Item;
-import se.citerus.cqrs.bookstore.shopping.domain.ProductId;
+import se.citerus.cqrs.bookstore.shopping.service.CartService;
 
 
 @RestController
@@ -32,13 +29,12 @@ import se.citerus.cqrs.bookstore.shopping.domain.ProductId;
 public class CartController {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
-	private final ProductCatalogClient productCatalogClient;
-	
+	private final CartService cartService;
 	private final CartRepository cartRepository;
 	
-	public CartController(ProductCatalogClient productCatalogClient, CartRepository cartRepository) {
-		this.productCatalogClient = productCatalogClient;
+	public CartController(CartRepository cartRepository, CartService cartService) {
 		this.cartRepository = cartRepository;
+		this.cartService = cartService;
 	}
 
 	@PostMapping
@@ -49,22 +45,14 @@ public class CartController {
 
 	@PostMapping("{cartId}/items")
 	public CartDto addItem(@PathVariable("cartId") String cartId, @RequestBody AddItemRequest addItemRequest) {
-		Cart cart = cartRepository.get(cartId);
-		logger.debug("Got addItem request " + addItemRequest);
-		ProductDto product = productCatalogClient.getProduct(addItemRequest.getProductId());
-		assertProductExists(addItemRequest.getProductId(), product);
-		Item item = new Item(new ProductId(addItemRequest.getProductId()), product.getBook().getTitle(), product.getPrice());
-		logger.info("Adding item to cart: " + item);
-		cart.add(item);
-		return CartDtoFactory.fromCart(cart);
+		return cartService.addItem(cartId, addItemRequest);
 	}
-	
+
 	@GetMapping("{cartId}")
-	public ResponseEntity<CartDto> getCart(@PathVariable("cartId") String cartId) {
+	public ResponseEntity<TransportObject> getCart(@PathVariable("cartId") String cartId) {
 		Cart cart = cartRepository.find(cartId);
 		if(cart == null) {
-			//TODO (String.format("Cart with id '%s' does not exist", cartId));
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiMessageDto(String.format("Cart with id '%s' does not exist", cartId)));
 		} else {
 			logger.info("Returning cart with [{}] lines", cart.getLineCount());
 			return ResponseEntity.ok().body(CartDtoFactory.fromCart(cart));
@@ -81,11 +69,5 @@ public class CartController {
 	public ResponseEntity<String> deleteAllCart() {
 		cartRepository.clear();
 		return ResponseEntity.ok("操作成功!");
-	}
-
-	private void assertProductExists(String productId, ProductDto product) {
-		if(product == null) {
-			throw new NullPointerException();
-		}
 	}
 }
---
+++ cqrs-bookstore/shopping-context/src/main/java/se/citerus/cqrs/bookstore/shopping/service/CartService.java
@@ -0,0 +1,38 @@
+package se.citerus.cqrs.bookstore.shopping.service;
+
+import com.google.common.base.Preconditions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import se.citerus.cqrs.bookstore.shopping.api.AddItemRequest;
+import se.citerus.cqrs.bookstore.shopping.api.CartDto;
+import se.citerus.cqrs.bookstore.shopping.api.CartDtoFactory;
+import se.citerus.cqrs.bookstore.shopping.client.productcatalog.ProductCatalogClient;
+import se.citerus.cqrs.bookstore.shopping.client.productcatalog.ProductDto;
+import se.citerus.cqrs.bookstore.shopping.domain.Cart;
+import se.citerus.cqrs.bookstore.shopping.domain.CartRepository;
+import se.citerus.cqrs.bookstore.shopping.domain.Item;
+import se.citerus.cqrs.bookstore.shopping.domain.ProductId;
+
+
+@Slf4j
+@Service
+public class CartService {
+    private CartRepository cartRepository;
+    private ProductCatalogClient productCatalogClient;
+
+    public CartService(CartRepository cartRepository, ProductCatalogClient productCatalogClient) {
+        this.cartRepository = cartRepository;
+        this.productCatalogClient = productCatalogClient;
+    }
+
+    public CartDto addItem(String cartId, AddItemRequest addItemRequest) {
+        Cart cart = cartRepository.get(cartId);
+        log.debug("Got addItem request " + addItemRequest);
+        ProductDto product = productCatalogClient.getProduct(addItemRequest.getProductId());
+        Preconditions.checkArgument(product != null && product.getBook() != null, String.format("不存在书籍"));
+        Item item = new Item(new ProductId(addItemRequest.getProductId()), product.getBook().getTitle(), product.getPrice());
+        log.info("Adding item to cart: " + item);
+        cart.add(item);
+        return CartDtoFactory.fromCart(cart);
+    }
+}
