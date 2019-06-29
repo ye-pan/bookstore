998e78f35adde5f9685a06cdda58d46d65010685
yepan <132842520@qq.com>
2019-06-29 08:53:43 +0800

    mvn springboot war适配

--- cqrs-bookstore/product-catalog-context/src/main/java/se/citerus/cqrs/bookstore/productcatalog/ProductCatalogContextApplication.java
+++ cqrs-bookstore/product-catalog-context/src/main/java/se/citerus/cqrs/bookstore/productcatalog/ProductCatalogContextApplication.java
@@ -2,9 +2,16 @@ package se.citerus.cqrs.bookstore.productcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductCatalogContextApplication {
+public class ProductCatalogContextApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.main(ProductCatalogContextApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCatalogContextApplication.class, args);
